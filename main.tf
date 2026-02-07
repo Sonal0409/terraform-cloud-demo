0efb00db07f184478cf954194f722e7244e9733f
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2026-02-07 22:22:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,22 +3,3 @@ region = "us-east-1"
 
 }
 
-data "aws_ami" "myami"{
-
-owners = ["amazon"]
-most_recent = true
-
-filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-
-}
-
-resource "aws_vpc" "sl-vpc" {
-  cidr_block       = "10.0.0.0/16"
-   tags = {
-    Name = "sl-vpc-01"
-  }
-}
