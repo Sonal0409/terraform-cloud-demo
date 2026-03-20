5a45c8af89ba70cfbed6a2dc6d3a484ecca384bd
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2026-03-20 16:00:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,40 +5,5 @@ region = "us-east-1"
 
 
 
-data "aws_ami" "myami"{
-
-owners = ["amazon"]
-most_recent = true
-
-filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-
-}
-
-resource "aws_instance" "myec2" {
-  ami    = "ami-02dfbd4ff395f2a1b" 
-  instance_type = "t3.micro"
-
-}
-
-
-
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
